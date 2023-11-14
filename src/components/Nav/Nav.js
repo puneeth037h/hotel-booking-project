5c57c7488aadf8b83d1ef416eda6f8751ff52656
puneeth037h <punithshanakanahalli@gmail.com>
2023-11-14 17:31:31 +0530

    nav bae update button update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,11 +3,13 @@ import "./Nav.css"
 function Nav(){
     return(
         <div className="Nav_bar">
-            <div>logo</div>
+            <div className="Logo">logo</div>
+            <div>
             <button className="Nav_buttons">Home</button>
             <button className="Nav_buttons">Hotels</button>
             <button className="Nav_buttons">About Us</button>
             <button className="Nav_buttons">Contact Us</button>
+            </div>
         </div>
     );
 }
