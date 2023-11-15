925599c3a970d23bf31ac9ffd5d602dd3d8433bf
puneeth037h <punithshanakanahalli@gmail.com>
2023-11-15 13:50:56 +0530

    features created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Herosection from './components/Hero section/Herosection';
+import Features from './components/Features/Features';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Nav></Nav>
       <Herosection></Herosection>
+      <Features></Features>
     </div>
   );
 }
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -2,8 +2,23 @@ import "./Features.css"
 
 function Features(){
     return(
-        <div>
-
+        <div className="Features-container">
+            <div>
+                <h2>Online Booking System:</h2>
+                <p>Implement a user-friendly and secure online booking system that allows guests to easily make reservations through the hotel's website or mobile app. Ensure that the process is intuitive, with clear instructions and visuals, making it convenient for guests to choose room types, select dates, and confirm their bookings.</p>
+            </div>
+            <div>
+                <h2>Room Variety and Amenities:</h2>
+                <p>Provide a range of room options to cater to different guest preferences and needs. Clearly showcase the amenities each room type offers, such as free Wi-Fi, air conditioning, in-room entertainment, complimentary toiletries, and other features that contribute to a comfortable stay.</p>
+            </div>
+            <div>
+                <h2>Customer Service and Concierge Services:</h2>
+                <p>Offer exceptional customer service to attend to guests' needs promptly. Consider implementing a concierge service that can assist with booking local tours, transportation, restaurant reservations, and other activities, providing a seamless and enjoyable stay.</p>
+            </div>
+            <div>
+                <h2>On-Site Dining Options:</h2>
+                <p>Include on-site dining options, such as a restaurant, café, or room service, offering diverse menus to accommodate different tastes and dietary preferences. Providing quality dining experiences adds convenience for guests who may prefer to dine within the hotel premises.</p>
+            </div>
         </div>
     );
 }
