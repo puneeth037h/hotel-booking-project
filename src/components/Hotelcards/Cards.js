89fa4b64ba074447d3f532eb19348d7e52f00186
puneeth037h <punithshanakanahalli@gmail.com>
2023-11-17 23:34:16 +0530

    card animation failed

--- src/components/Hotelcards/Cards.js
+++ src/components/Hotelcards/Cards.js
@@ -1,8 +1,36 @@
 import './Cards.css'
+import { useEffect } from 'react';
 import arrow from '../../Assets/icons1/right-arrow.png'
+import star from '../../Assets/icons1/star.png'
 
 function Cards({hotel}){
+    useEffect(() => {
+        const card = document.querySelector('.card-container');
+        const cardImage = document.querySelector('.cardThumbnail');
     
+        const handleMouseEnter = () => {
+          cardImage.style.transform = 'translateY(-40px)';
+        };
+    
+        if (card) {
+          card.addEventListener('mouseenter', handleMouseEnter);
+    
+          // Cleanup the event listener when the component unmounts
+          return () => {
+            card.removeEventListener('mouseenter', handleMouseEnter);
+          };
+        }
+      }, []);
+
+    // let card=document.querySelector('.card-container');
+
+    // let cardimage = document.querySelector('.cardThumbnail');
+
+    // card.addEventListener('mouseenter', ()=>{cardimage.Style.transform = 'translateY(-40px)';})
+
+
+
+
     return(
         <div className='Crad-container'>
             <div >
@@ -17,7 +45,7 @@ function Cards({hotel}){
                         <p>Starting from :{hotel.startingPrice} INR</p>
                     </div>
                     <div>
-                        <p>Rating:{hotel.rating}</p>
+                        <p>Rating:{hotel.rating} <img src={star} className="rating-star"alt="" /></p>
                         <button className='card-button'>
                                 <span>View</span>
                                 <img src={arrow} alt="arrow" />
