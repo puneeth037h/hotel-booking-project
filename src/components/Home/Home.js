4907c212e023b58cf8827539888fd3df549e438b
puneeth037h <punithshanakanahalli@gmail.com>
2023-11-21 13:54:52 +0530

    VIEW PAGE created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,8 @@ import Features from "../Features/Features.js";
 import Featuredhotel from "../FeaturedHotels/FeaturedHotels.js";
 import "./Home.css"
 import Footer from "../Footer/Footer.js";
+import { Link } from "react-router-dom";
+import arrow from '../../Assets/icons1/right-arrow.png'
 
 function Home(){
     return(
@@ -11,6 +13,12 @@ function Home(){
         <div className="Featuredhotel-container">
             <h1>Featured Hotel</h1>
             <Featuredhotel></Featuredhotel>
+            <Link to={'/Hotels'}>    
+                <button className='viewmore-button'>
+                    <span>view more</span>
+                    <img src={arrow} alt="arrow" />
+                </button>
+            </Link>
         </div>
         <div className="Hotel_features">
             <h1>Features</h1>
--- src/components/Hotel_details/Hotel_details.js
+++ src/components/Hotel_details/Hotel_details.js
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./Hotel_details.css"
 
 function ViewHoteldetails(){
     let { name } = useParams();
@@ -29,7 +30,28 @@ function ViewHoteldetails(){
     },[]);
     return(
         <div>
-            <h1>{hoteldetails.city}</h1>
+            <div className="Hotel_name">
+                <h1>{hoteldetails.name}</h1>
+            </div>
+            <div className="Hotel_location">
+                <h2>{hoteldetails.city},{hoteldetails.state}</h2>
+                {/* <h2>address:{hoteldetails.address}</h2> */}
+            </div>
+            <div className="Hotel_images">
+                <img className="hotel-thumbnail" src={hoteldetails.thumbnail} alt="" />
+            </div>
+            <div className="pricing-rating-container" >
+                <div className="pricing-container">
+                    <h1>Starting at {hoteldetails.starting_price}</h1>
+                    <button>Book now</button>
+                </div>
+                <h2>{hoteldetails.rating}</h2>
+            </div>
+            
+            
+            <h1>contact</h1>
+            <h2>Phone:{hoteldetails.phone}</h2>
+            <h2>Email:{hoteldetails.email}</h2>
         </div>
 
     );
